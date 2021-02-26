49ae812c2b48766db991861025e2e30e6823341f
Carlos Arias <43482712+carlosaariasl@users.noreply.github.com>
2021-02-25 23:20:41 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,46 @@
+#Operaciones enteras:
+casa = 10
+via = 15
+print ('casa' + 'via')
+print(type)
+
+#Practica con módulo
+print('Division x modulo 4%2', 4%2)
+print ('Division x modulo 3%2', 3%2)
+
+
+#Numeros negativos division entera
+print('División verdadera 3/2:', 3/2)
+print ('División verdadera 3//2:', -3/2)
+print('Entera división 3//2:',3//2)
+print('Entera división 3//2:', -3//2)
+
+#Operaciones con Numeros flotantes
+print(2.3+1.5)
+print(1.5/2.3)
+print(1.5*2.3)
+print(2.3-1.5)
+print(1.5-2.3)
+
+
+""" 
+#Operaciones flotantes
+print(100/20)
+print(type(100))
+
+#Devolver solo la parte entera
+res1 = 3//2
+print(res1)
+print(type(res1))
+
+
+#Interos y flotantes
+
+i = 3 * 0.1
+print(i)
+
+#Complex Nuber Operator
+c1 = 1j
+c2 = 2j
+c3 = c1 * c2
+print(c3) """
---
+++ tipospyton.py
@@ -0,0 +1,33 @@
+# Comentarios
+
+# Tipos de datos
+# String
+# Primer programa Hola Mundo
+print("Hola Mundo")
+
+# Integer
+print(30)
+
+# Float
+print(30.3)
+
+# Boolean
+True
+False
+
+# List
+[10,20,30,55]
+["Hello",'Ohh','Yeah',"World"]
+[10,"Hello",True,10.1]
+
+# Tuples
+(10,30,20,40)
+()
+
+# Directories
+print(type({
+    "name":"Programación III", 
+    "lastname":"UMG",
+    "nickname":"Central"
+})
+)
