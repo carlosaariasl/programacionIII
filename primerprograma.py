8697ce9eb6a19e03abe7ebdce8326f75381721d1
Carlos Arias <43482712+carlosaariasl@users.noreply.github.com>
2023-01-19 12:58:35 -0600

    Add files via upload

---
+++ primerprograma.py
@@ -0,0 +1,33 @@
+# Ejercicio 1: Hola Mundo
+
+# print("Hola Mundo")
+
+# Ejercicio 2: Programa que almacene una cadena
+
+# mensaje = "Hola Mundo"
+# print(mensaje)
+
+# Ejercicio 3:  Escribir un programa que muestre el nombre de usuario en consola
+
+# nombre = input("Introduce tu nombre: ")
+# print("Hola bienvenido " + nombre)
+
+#Ejercicio 4: Escribir en pantalla un programa que muestre en pantalla una opción aritmética (3+2/2*5) elevado 2
+
+#print (((3+2)/(2*5))**2)
+
+#Ejercicio 5: Escribir un programa que pregunte el usuario por el numero de horas trabajadas y el coste por hora. 
+#Despues debe mostrar en pantalla lo que le corresponde de pago.
+
+# nombre = input("Introduce tu nombre: ")
+# horas = float(input("Introduce tus horas de trabajo: "))
+# costo = float (input("Introduce lo que ganas por hora: "))
+# pago = horas * costo
+# print("Tu paga es de ", pago, "Quetzales")
+
+
+texto = "hola mundo"
+textoenMayuscula = texto.capitalize()
+print(textoenMayuscula)
+
+
